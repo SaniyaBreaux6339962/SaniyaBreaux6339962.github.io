503daa46700eafedd7ab12c03777fb6b2647dfd8
SaniyaBreaux6339962 <srb6339962@lpssonline.com>
2026-03-31 22:22:50 +0000

    todo 7

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -41,12 +41,23 @@ drawCircle()
 drawCircle()
 
         // TODO 7 : Use a loop to create multiple circles
-    
+     drawCircle()
 
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+    for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
+
+            drawCircle();
+
+        }
         
         }
-
-
+     
 
         ///////////////////
         // PROGRAM LOGIC //
@@ -59,11 +70,7 @@ drawCircle()
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-        physikz.updatePosition(circles[ 0 ])
-physikz.updatePosition(circles[ 1 ])
-physikz.updatePosition(circles[ 2 ])
-physikz.updatePosition(circles[ 3 ])
-physikz.updatePosition(circles[ 4 ])
+        
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
            game.checkCirclePosition(circles[ 0 ])
@@ -74,7 +81,15 @@ game.checkCirclePosition(circles[ 3 ])
 game.checkCirclePosition(circles[ 4 ])
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+
+               physikz.updatePosition(circles[i]);
+game.checkCirclePosition(circles[i]);
+
+
+}
+
+
 
 }
         
